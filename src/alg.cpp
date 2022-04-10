b9b076473ea28005dcba04839aeeeff3e3f96efd
21IST2Flerov <99124529+21IST2Flerov@users.noreply.github.com>
2022-04-10 16:18:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,113 +1,38 @@
 // Copyright 2021 NNTU-CS
-#include <string>
-#include <map>
-#include <stack>
-#include "tstack.h"
-int prior(char c) {
-  switch (c) {
-    case '(':
-      return 0;
-    case ')':
-      return 1;
-    case '+':
-      return 2;
-    case '-':
-      return 2;
-    case '*':
-      return 3;
-    case '/':
-      return 3;
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+template<typename T, int size>
+class TStack {
+ private:
+  T arr[100];
+  int top;
+ public:
+  TStack() : top(-1) {}
+  bool isEmpty() const {
+    return top == -1;
   }
-  return 0;
-}
-bool search(std::string s, char c) {
-  for (int i = 0; i < s.length(); ++i) {
-    if (s[i] == c) {
-      return true;
-    }
+
+  bool isFull() const {
+    return top == size - 1;
   }
-  return false;
-}
-std::string infx2pstfx(std::string inf) {
-  TStack<char, 100> stack;
-  std::stack<char> Stack;
-  std::string number = "0123456789";
-  std::string operation = "()+-*/";
-  std::string result = "";
-  for (int i = 0; i < inf.length(); ++i) {
-    if (search(number, inf[i])) {
-      result += inf[i];
-      result += ' ';
-    } else if (search(operation, inf[i])) {
-      if (prior(inf[i]) == 0) {
-        Stack.push(inf[i]);
-      } else if (Stack.empty()) {
-        Stack.push(inf[i]);
-      } else if (prior(inf[i]) > prior(Stack.top())) {
-        Stack.push(inf[i]);
-      } else if (inf[i] == ')') {
-        while (Stack.top() != '(') {
-          result += Stack.top();
-          result += ' ';
-          Stack.pop();
-        }
-        if (Stack.top() == '(') {
-          Stack.pop();
-        }
-      } else {
-        for (int j = 0; j<= Stack.size(); ++j) {
-          if (prior(Stack.top()) >= prior(inf[i])) {
-            result += Stack.top();
-            result += ' ';
-            Stack.pop();
-          }
-        }
-        Stack.push(inf[i]);
-      }
-    }
+
+  T get() {
+    if (!isEmpty())
+      return *(arr + top);
+    else
+      throw "Stack is empty yet, add more elements";
   }
-  if (!Stack.empty()) {
-    for (int j = 0; j <= Stack.size(); ++j) {
-      result += Stack.top();
-      result += ' ';
-      Stack.pop();
-    }
+  void pop() {
+    if (!isEmpty())
+      --top;
   }
-  
-  if (result[result.length() - 1] == ' ') {
-    result.erase(result.length() - 1);
+  void push(T value) {
+    if (!isFull())
+      *(arr + ++top) = value;
+    else
+      throw "Stack is already full, remove some elements";
   }
-  return result;
-}
-int eval(std::string pref) {
-  int sum = 0;
-  std::stack<int> Stack;
-  TStack<int, 100> stack;
-  std::string number = "0123456789";
-  std::string operation = "()+-*/";
-  for (int i = 0; i < pref.length(); ++i) {
-    if (search(number, pref[i])) {
-      Stack.push(pref[i] - '0');
-    } else if (search(operation, pref[i])) {
-      int a = 0, b = 0;
-      a = Stack.top();
-      Stack.pop();
-      b = Stack.top();
-      Stack.pop();
-      if (pref[i] == '+') {
-        Stack.push(b + a);
-      }
-      if (pref[i] == '-') {
-        Stack.push(b - a);
-      }
-      if (pref[i] == '*') {
-        Stack.push(b * a);
-      }
-      if (pref[i] == '/') {
-        Stack.push(b / a);
-      }
-    }
-  }
-  sum = Stack.top();
-  return sum;
-}
+};
+#endif  // INCLUDE_TSTACK_H_
+TStack<char, 100> stack1;
+TStack<int, 100> stack2;
